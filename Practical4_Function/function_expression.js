02cf36b26e3147a4744ef0de0662aea3d91b0472
Shreenarayan <shreenarayanlove123@gmail.com>
2023-04-04 12:59:07 +0545

    Adding function_expression

---
+++ Practical4_Function/function_expression.js
@@ -0,0 +1,18 @@
+//Function Expression Approach
+let add=function(a,b){
+    return a+b;
+}
+let result=add(5,6);
+console.log('Sum of two number is ' + result);
+
+function sum(a,b){
+    return a+b;
+}
+var result1=sum(3,4);
+console.log('Sum of two number is '+ result1);
+
+var sum_and_multiply=function(x,y){
+    return x * y;
+}
+var sum_and_multiply_result=sum_and_multiply(result1,2);
+console.log(sum_and_multiply_result);
